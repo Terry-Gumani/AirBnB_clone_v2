3e6d122eb9339e4b0b4c87ee14970fcaf3dd8c88
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-19 22:36:52 +0000

    write setup test database and doc modifications for dev db setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,4 @@
--- Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
---   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev on performance_schema.
+--  a script that prepares a MySQL dev server for the project:
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
