0d6299f8c2bd3786d544c88c3d0a4229f4c1e566
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-20 19:30:46 +0000

    commit part of the changes to models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,8 @@ import uuid
 from datetime import datetime
 
 
+Base = declarative_base()
+
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
@@ -13,7 +15,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +32,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -42,3 +44,8 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """delete current instance from storage"""
+        from models import storage
+        storage.delete(self)
