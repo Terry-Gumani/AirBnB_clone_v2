0bc35ccca8bee9717f1e5b3ac6b30607d07ef114
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-21 00:17:01 +0000

    changes to models/city.py and models/state.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    isdb = storage_type == 'db'
+    name = Column(string(128), nullable=False) if isdb else ""
+    state_id = Column(string(60),
+                      ForeignKey('states.id'),
+                      nullable=False) if isdb else ""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import city
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class/ table model """
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False) if storage_type == 'db' else ""
+
+        @property
+        def cities(self):
+            from models import storage
+            related_cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
